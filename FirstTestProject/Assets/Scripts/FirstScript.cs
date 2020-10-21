d61f78146bb92443635ee62669433b90acfe1644
travissciberras <73162283+travissciberras@users.noreply.github.com>
2020-10-21 14:25:22 +0200

    Created first Unity project
    Difference between start and update methods.

---
+++ FirstTestProject/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //The line will be outputted once before the first frame update
+        print("Welcome SWD4.2B");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The line will be outputted with every frame (Example: if running at 30FPS, the line will be outputted 30 times per second
+        print("Hello!");
+    }
+}
